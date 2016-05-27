2335a498c0443a6bbd0f084b40514c592761b049
Maxim Pervushin <maxim.pervushin@gmail.com>
2016-05-27 14:50:20 +0300

    About page for iOS added.

---
+++ iOS/Source/About/AboutViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UIKit;
+
+namespace FitCalculator.iOS
+{
+	public partial class AboutViewController : UITableViewController
+	{
+		public AboutViewController (IntPtr handle) : base (handle)
+		{
+			// @{UITextAttributeTextColor: [UIColor whiteColor]}
+
+//			UINavigationItem.Appearance.SetTitleTextAttributes ();
+		}
+	}
+}
